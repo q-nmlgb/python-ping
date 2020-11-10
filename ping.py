cfed89da06bfcfbb90e630c747736e80775a23e8
tattoomvp <71119932+tattoomvp@users.noreply.github.com>
2020-11-10 23:30:30 +0800

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,7 +4,7 @@ from queue import Queue
 import threading
 from subprocess import Popen,PIPE
 
-class doRun(threading.Thread):
+class doRun(threading.Thread):                       #继承线程对象
     def __init__(self,queue):
         threading.Thread.__init__(self)
         self._queue=queue
@@ -12,24 +12,24 @@ class doRun(threading.Thread):
         while not self._queue.empty():
             ip=self._queue.get()
             # print(ip)
-            ping=Popen('ping %s' % ip,shell=True,stdin=PIPE,stdout=PIPE)
-            rus=ping.stdout.read().decode('gbk')
+            ping=Popen('ping %s' % ip,shell=True,stdin=PIPE,stdout=PIPE)   #cmd命令对象
+            rus=ping.stdout.read().decode('gbk')                            #读取输出并解码为gbk
             if 'TTL' in rus:
-                print(ip,'is up')
+                print(ip,'is up')                                           
             else:
                 print(ip,'is down')
 
-def main():
-    thread=[]
-    count=10
-    q=Queue()
+def main():                                     
+    thread=[]                                    #创建线程空列表
+    count=10                                     #线程数
+    q=Queue()                                    #实例队列
     
     for i in range(1,255):
-        q.put('222.138.1.'+str(i))
+        q.put('222.138.1.'+str(i))               #put所有ip放入实例队列
     for i in range(count):
-        thread.append(doRun(q))  
+        thread.append(doRun(q))                  #实例线程对象加入线程列表  
     for i in thread:  
-        i.start()
+        i.start()                                   
     for i in thread:  
         i.join()
 
